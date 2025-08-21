67601e59df85516746228e27daf30696b2e6ae1a
arturican <aturican@gmail.com>
2025-08-21 16:54:27 +0300

    добавляю редьюсер и тесты для проекта

---
+++ src/model/todolists-reducer.ts
@@ -0,0 +1,19 @@
+import type { Todolist } from '../App.tsx';
+
+export type DeleteTodolistAction = {
+  type: 'delete_todolist';
+  payload: {
+    id: string;
+  };
+};
+type Actions = DeleteTodolistAction;
+const initialState: Todolist[] = [];
+export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
+  switch (action.type) {
+    case 'delete_todolist': {
+      return state.filter(todolist => todolist.id !== action.payload.id);
+    }
+    default:
+      return state;
+  }
+};
