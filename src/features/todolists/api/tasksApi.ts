aa86072b7e7fbe5305423e6cadd6dff3a4c56ae9
arturican <arturican@gmail.com>
2025-10-30 13:03:30 +0300

    добавил тип для createTask

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,11 +1,14 @@
 import { instance } from '@/common/instance/instance.ts';
-import type { GetTasksResponse } from '@/features/todolists/api/tasksApi.types.ts';
+import type { DomainTask, GetTasksResponse } from '@/features/todolists/api/tasksApi.types.ts';
+import type { BaseResponse } from '@/common/types/types.ts';
 
 export const tasksApi = {
   getTasks(todolistId: string) {
     return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`);
   },
   createTask({ todolistId, title }: { todolistId: string; title: string }) {
-    return instance.post(`/todo-lists/${todolistId}/tasks`, { title });
+    return instance.post<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${todolistId}/tasks`, {
+      title,
+    });
   },
 };
