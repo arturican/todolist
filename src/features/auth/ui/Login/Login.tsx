6ed27f66b09af93244e072ae2095b9bd688a4844
arturican <arturican@gmail.com>
2025-11-25 10:26:20 +0300

    добавил форму для Login

--- src/features/auth/ui/Login/Login.tsx
+++ src/features/auth/ui/Login/Login.tsx
@@ -10,44 +10,100 @@ import TextField from '@mui/material/TextField';
 import { useAppSelector } from '@/common/hooks/useAppSelector';
 import { getTheme } from '@/common/theme/theme';
 import Grid from '@mui/material/Grid';
+import { Controller, type SubmitHandler, useForm } from 'react-hook-form';
+import styles from './Login.module.css';
+
+type LoginInputs = {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+};
 
 export const Login = () => {
   const themeMode = useAppSelector(selectThemeMode);
 
   const theme = getTheme(themeMode);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm<LoginInputs>({
+    defaultValues: { email: '', password: '', rememberMe: false },
+  });
+
+  const onSubmit: SubmitHandler<LoginInputs> = data => {
+    console.log(data);
+    reset();
+  };
 
   return (
     <Grid container justifyContent={'center'}>
-      <FormControl>
-        <FormLabel>
-          <p>
-            To login get registered
-            <a
-              style={{ color: theme.palette.primary.main, marginLeft: '5px' }}
-              href="https://social-network.samuraijs.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              here
-            </a>
-          </p>
-          <p>or use common test account credentials:</p>
-          <p>
-            <b>Email:</b> free@samuraijs.com
-          </p>
-          <p>
-            <b>Password:</b> free
-          </p>
-        </FormLabel>
-        <FormGroup>
-          <TextField label="Email" margin="normal" />
-          <TextField type="password" label="Password" margin="normal" />
-          <FormControlLabel label="Remember me" control={<Checkbox />} />
-          <Button type="submit" variant="contained" color="primary">
-            Login
-          </Button>
-        </FormGroup>
-      </FormControl>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl>
+          <FormLabel>
+            <p>
+              To login get registered
+              <a
+                style={{ color: theme.palette.primary.main, marginLeft: '5px' }}
+                href="https://social-network.samuraijs.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>
+            </p>
+            <p>or use common test account credentials:</p>
+            <p>
+              <b>Email:</b> free@samuraijs.com
+            </p>
+            <p>
+              <b>Password:</b> free
+            </p>
+          </FormLabel>
+          <FormGroup>
+            <TextField
+              label="Email"
+              margin="normal"
+              error={!!errors.email}
+              {...register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: 'Incorrect email address',
+                },
+              })}
+            />
+            {errors.email && (
+              <span className={styles.errorMessage}>
+                {errors.email.message}
+              </span>
+            )}
+            <TextField
+              type="password"
+              label="Password"
+              margin="normal"
+              {...register('password')}
+            />
+            <FormControlLabel
+              label="Remember me"
+              control={
+                <Controller
+                  name="rememberMe"
+                  control={control}
+                  render={({ field: { value, ...rest } }) => (
+                    <Checkbox {...rest} checked={value} />
+                  )}
+                />
+              }
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Login
+            </Button>
+          </FormGroup>
+        </FormControl>
+      </form>
     </Grid>
   );
 };
