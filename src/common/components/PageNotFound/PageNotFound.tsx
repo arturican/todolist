510a62b7e59662009a6c2e6e806c14be9a2656f5
arturican <arturican@gmail.com>
2025-11-27 16:24:14 +0300

    добавил кнопку возращения с компоненты PageNotFound

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router';
 import styles from './PageNotFound.module.css';
+import Button from '@mui/material/Button';
 export const PageNotFound = () => (
   <>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
+    <Button
+      component={Link}
+      to="/"
+      variant={'contained'}
+      color={'primary'}
+      sx={{
+        maxWidth: 400,
+        margin: 'auto',
+      }}
+    >
+      Вернуться на главную
+    </Button>
   </>
 );
