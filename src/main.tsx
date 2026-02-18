3644674b0350dff1db597b275023285102598905
arturican <arturican@gmail.com>
2026-02-18 20:06:06 +0300

    сделал ссылки для портфолио

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { App } from '@/app/App.tsx';
 import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/todolist">
     <Provider store={store}>
       <App />
     </Provider>
